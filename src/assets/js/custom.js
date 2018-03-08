0ab00bc3d46bddc42661c2eb5b8c951a8c7ed990
O'Brien, Corey (US - Camp Hill) <coobrien@deloitte.com>
2018-03-08 17:40:47 -0500

    Changes to support custom cards for AccountInsights.

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,6 +1,19 @@
 /*
 	Custom App Javascripts
-	Created on 11/20/2017 by Corey O'Brien.
+	Created on 02/12/2018 by Corey O'Brien.
 */
 
-// Custom JS Goes Here.
+$(document).ready(function () {
+
+	// start script for toggling the size of a card.
+	$.each($('.toggleFullscreen'), function (key, value) {
+		$(value).click(function(e){
+			$(value).parent().parent().parent().toggleClass('fullscreen');
+			$('.card-task').parent().toggleClass('col-lg-12');
+			$('body').toggleClass('no-scroll');
+		});
+	});
+	// end script for toggling the size of a card.
+	
+});
+	
