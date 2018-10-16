c445d9861579cb0efe960e666fb17a2c5951f7d0
Rengarajan <vrengarajan@deloitte.com>
2018-10-16 16:48:25 -0400

    Commiting changes done for form group component

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import DPBanner from "./dp-banner.vue";
-import DPText from "./dp-text.vue";
-
+import DPText from "./dp-form-inputs/dp-text.vue";
+import DPLabel from "./dp-form-inputs/dp-label.vue";
+import DPFormGroup from "./dp-form-inputs/dp-form-group.vue";
 const Components = {
   DPBanner,
-  DPText
+  DPText,
+  DPLabel,
+  DPFormGroup
 };
 
 Object.keys(Components).forEach(name => {
