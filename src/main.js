207590ffc04b2c5e4840c2637a90ba9c6e6769e1
O'Brien, Corey (US - Harrisburg) <coobrien@deloitte.com>
2018-10-17 10:34:48 -0400

    Work on getting the CSS up and running for the pattern library styles.

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import './styles.scss'
 
 import {routes} from './routes';
 import './components'
