c36445ea20a721b91e77af0dfcaaef4b012f4adf
O'Brien, Corey (US - Harrisburg) <coobrien@deloitte.com>
2018-11-04 15:10:09 -0500

    Added btn-group control.

---
+++ src/components/dp-actions/dp-btn-group.vue
@@ -0,0 +1,12 @@
+<template>
+    <b-button-group></b-button-group>
+</template>
+<script>
+    import bButtonGroup from 'bootstrap-vue/es/components/button-group/button-group';
+
+    export default {
+        extends: bButtonGroup,
+    };
+</script>
+
+
