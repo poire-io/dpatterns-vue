e70069a8320659c35b1dca0bb32172e1b3aa7303
O'Brien, Corey (US - Harrisburg) <coobrien@deloitte.com>
2018-11-05 12:28:20 -0500

    Adjustments to the form controls for the pattern library.

--- src/components/dp-form-inputs/dp-form-group.vue
+++ src/components/dp-form-inputs/dp-form-group.vue
@@ -1,63 +1,18 @@
 <template>
-  <div class="form-group">
-    <dpLabel v-if="'label!=null'" :label="label" :id="id"/>
-    <dpText v-if="type==='text'" :id="id" :name="name" :value="value" :maxlength="max" :icon="icon" :type="type" :placeholder="placeholder" :label="label" :state="state" v-on:input="onInput"/>
-    <b-form-invalid-feedback :id="'inputFeedback_' + id">
-      {{errorMsg}}
-    </b-form-invalid-feedback>
-    <b-form-text :id="'inputHelp_' + id">
-      {{helpTxt}}
-    </b-form-text>
-  </div>   
+    <b-form-group v-bind="$props">
+        <slot></slot>
+    </b-form-group>
 </template>
+
 <script>
-  export default {
-    props:{
-      id:{
-        type: String
-      },
-      label:{
-        type: String
-      },
-      placeholder: {
-        type: String
-      },
-      max: {
-        type: Number
-      },
-      validate: {
-        type: [String, Object]
-      },
-      value: {
-        type: String
-      },
-      message: {
-        type: String
-      },
-      icon: {
-        type: String
-      },
-      name: {
-        type: String
-      },
-      type: {
-        type: String
-      },
-      errorMsg:{
-          type: String
-      },
-      helpTxt:{
-          type: String
-      },
-      state:{
-          type: Boolean, 
-          default: null
-      }
-    },
-    methods: {
-      onInput(value) {
-        this.$emit("input", value);
-      }
-    } 
-  }
+    import bFormGroup from 'bootstrap-vue/es/components/form-group/form-group';
+    
+    const btnProps = Object.assign({}, bFormGroup.props);
+
+    export default {
+        components: { bFormGroup },
+        props: {
+            ...btnProps,
+        }
+    }
 </script>
--- src/pages/FormControls.vue
+++ src/pages/FormControls.vue
@@ -8,27 +8,77 @@
           <div class="card">
             <div class="card-body">
               <h2 class="card-title">Input Controls</h2>
+              <h3>Text Fields</h3>
               <div class="form-row">
                 <div class="col-4">
-                  <dpLabel label="Label Sample" id="textInput"/>
-                  <dpText type="text" id="textInput" label="Text type" name="Text type" :max=4 placeholder="Input Text Field." :value="value"  v-model="value" icon="Search" />
+                  <dpLabel label="Single Controls" id="textInput"/>
+                  <dpText type="text" id="textInput" label="Text type" name="Text type" :max=4 placeholder="Input Text Field." :value="value"  v-model="value" />
                 </div>
                 <div class="col-4">
-                  <dpFormGroup type="text" id="txtType" label="Text type" name="Text type" :max=5 placeholder="Valid Text Group." :value="validValue"  v-model="validValue" icon="Search" helpTxt="Help Text display" :state="true"/>
+                  <dpFormGroup id="lblExample2" description="Help text here." label="Text Field With Group" label-for="txtExample2">
+                    <b-form-input id="txtExample2"></b-form-input>
+                  </dpFormGroup>
                 </div>
                 <div class="col-4">
-                  <dpFormGroup type="text" id="txtType2"  label="Text type" name="Text type" :max=6 placeholder="Invalid Text Group." :value="invalidValue"  v-model="invalidValue" icon="Search" errorMsg="Validation error" helpTxt="Help Text display" :state="false"/>
+                  <dpFormGroup id="lblExample3" label="Field With Error" label-for="txtExample3" invalid-feedback="Validation error">
+                    <b-form-input id="txtExample3" :value="invalidValue" v-model="invalidValue" icon="Search" placeholder="Invalid Text Group." :state="false"></b-form-input>
+                  </dpFormGroup>
                 </div>
               </div>
+              <hr>
+              <h3>Radios and Checkboxes</h3>
               <div class="form-row">
+                <div class="col-4">
+                  <dpFormGroup id="lblExample4" label="Radios In Field Group">
+                    <b-form-radio-group class="pt-2" :options="['Radio 1', 'Radio 2', 'Radio 3']" />
+                  </dpFormGroup>
+                </div>
+                <div class="col-4">
+                  <dpFormGroup id="lblExample4" label="Checkboxes In Field Group">
+                    <b-form-checkbox-group class="pt-2" :options="['Check 1', 'Check 2', 'Check 3']" />
+                  </dpFormGroup>
+                </div>
+              </div>
+              <hr>
+              <h3>Dropdowns</h3>
+              <div class="form-row">
+                <div class="col-4">
+                  <dpFormGroup id="lblExample4" label="Dropdown">
+                    <b-form-select :options="options" class="mb-3" />
+                  </dpFormGroup>
+                </div>
+              </div>
+              <!-- <div class="form-row">
                 <div class="col-4">
                   <dpLabel label="Sample Autocomplete" id="txtAutoComplete" />
                   <dpAutoComplete :items=states placeholder="Testing" />
                 </div>
+              </div> -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-body">
+              <h2 class="card-title">Actions</h2>
+              <h3>Button Types</h3>
+              <div class="row">
+                <div class="col-12">
+                  <dpButton variant="primary">Primary Button</dpButton>
+                  <dpButton variant="danger">Danger Button</dpButton>
+                  <dpButton variant="warning">Warning Button</dpButton>
+                  <dpButton variant="success">Success Button</dpButton>
+                  <dpButton variant="outline-success">Success Outline Button</dpButton>
+                  <dpButton variant="outline-primary">Primary Outline Button</dpButton>
+                </div>
               </div>
-              <div class="row mt-4">
-                <div class="col-4">
-                  <dpBtnGroup>
+              <hr>
+              <h3>Buttons Inside Button Bar</h3>
+              <div class="row">
+                <div class="col-12">
+                  <dpBtnGroup class="action-bar">
                     <dpButton variant="primary">Primary Button</dpButton>
                     <dpButton variant="warning">Warning Button</dpButton>
                     <dpButton variant="danger">Danger Button</dpButton>
@@ -63,7 +113,14 @@
         value: null,
         validValue: null,
         invalidValue: null,
-        states: this.states
+        states: this.states,
+        options: [
+          { value: null, text: 'Please select an option' },
+          { value: 'a', text: 'This is First option' },
+          { value: 'b', text: 'Selected Option' },
+          { value: {'C': '3PO'}, text: 'This is an option with object value' },
+          { value: 'd', text: 'This one is disabled', disabled: true }
+        ]
       }
     }
   }
